734b9033e2f66624dc1803ee02704ed2440ec349
Faper <43310141+Seo-Faper@users.noreply.github.com>
2021-06-27 22:33:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 var express = require('express')
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 var app = express()
 
-
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
+    if(_url == '/'){
+      title = 'Welcome';
+    }
+    if(_url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    var template = `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+        <canvas id="screen" width="680" height="420"></canvas>
+    </body>
+    </html>
+    `;
+    response.end(template);
+ 
+});
 app.listen(8080, function() {
     console.log("[GSS CERT] Server ON")
-})
-
-app.get('/', function(req,res) {
-    res.send("<h1>Welcome</h1>")
-})
+});
